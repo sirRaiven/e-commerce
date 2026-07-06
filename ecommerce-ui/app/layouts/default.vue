55f3e51ad5bc6afcc8887521889c9d5fec62da93
sirRaiven <sirraivensupan@gmail.com>
2026-07-06 20:10:58 +0800

    Remove navigation bar from default layout
    Removed navigation bar from the default layout.

--- ecommerce-ui/app/layouts/default.vue
+++ ecommerce-ui/app/layouts/default.vue
@@ -1,17 +1,5 @@
 <template>
   <v-app>
-    <!-- Navigation Bar -->
-    <v-app-bar color="primary" elevation="2">
-      <v-app-bar-title class="font-weight-bold">
-        Simple E-Commerce
-      </v-app-bar-title>
-
-      <v-spacer />
-
-      <v-btn to="/" variant="text"> Products </v-btn>
-
-      <v-btn to="/pos" variant="text"> POS </v-btn>
-    </v-app-bar>
 
     <!-- Main Content -->
     <v-main class="bg-grey-lighten-4">
